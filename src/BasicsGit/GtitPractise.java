aa8d01d9ad281e972fa8265076e202def723902a
naveenreddyanuti@gmail.com <naveenreddyanuti@gmail.com>
2021-02-12 12:54:57 +0530

    First commit

---
+++ src/BasicsGit/GtitPractise.java
@@ -0,0 +1,14 @@
+package BasicsGit;
+
+public class GtitPractise {
+
+	public static void main(String[] args) {
+		int a=10, b =20;
+		a = a+b;
+		b = a-b;
+		a = a-b;
+		System.out.print("Swapping two numbers:"+a+ " "+b);
+		
+ 	}
+
+}
