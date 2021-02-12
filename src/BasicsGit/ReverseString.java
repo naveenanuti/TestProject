42b6632a949536414e61c9929c185b0f4e70df94
naveenreddyanuti@gmail.com <naveenreddyanuti@gmail.com>
2021-02-12 13:31:31 +0530

    Second commit

---
+++ src/BasicsGit/ReverseString.java
@@ -0,0 +1,15 @@
+package BasicsGit;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String s = "Naveen";
+		for(int i= s.length()-1;i>=0;i--) {
+			
+			
+			System.out.print(s.charAt(i));
+		}
+		}
+	}
+
+
